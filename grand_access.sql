2571cddc1af04ba2912cdaff21a92b98c6c51e5d
mmaz <marta@mazurkiewi.cz>
2021-12-26 14:17:20 +0100

    Added procedure grand_access

---
+++ grand_access.sql
@@ -0,0 +1,12 @@
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE PROCEDURE grand_access (os_path VARCHAR2) 
+IS dirSQL varchar2(4000);
+BEGIN 
+dirSQL := 'CREATE OR REPLACE DIRECTORY report_dir AS ''' || os_path || '''';
+EXECUTE IMMEDIATE(dirSQL);
+dirSQL := 'GRANT READ, WRITE ON DIRECTORY report_dir TO perfstat';
+EXECUTE IMMEDIATE(dirSQL);
+EXCEPTION
+WHEN OTHERS THEN dbms_output.put_line(SQLERRM);
+END;
+/
