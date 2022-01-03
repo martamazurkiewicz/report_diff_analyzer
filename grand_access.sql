1941b592d13af21c77e0ca26d1a6fe6f1381116f
mmaz <marta@mazurkiewi.cz>
2022-01-03 07:01:17 +0100

    Formatted proc

--- grand_access.sql
+++ grand_access.sql
@@ -1,12 +1,14 @@
 SET SERVEROUTPUT ON;
 CREATE OR REPLACE PROCEDURE grand_access (os_path VARCHAR2) 
-IS dirSQL varchar2(4000);
+IS 
+    dirSQL varchar2(4000);
 BEGIN 
-dirSQL := 'CREATE OR REPLACE DIRECTORY report_dir AS ''' || os_path || '''';
-EXECUTE IMMEDIATE(dirSQL);
-dirSQL := 'GRANT READ, WRITE ON DIRECTORY report_dir TO perfstat';
-EXECUTE IMMEDIATE(dirSQL);
+    dirSQL := 'CREATE OR REPLACE DIRECTORY report_dir AS ''' || os_path || '''';
+    EXECUTE IMMEDIATE(dirSQL);
+    dirSQL := 'GRANT READ, WRITE ON DIRECTORY report_dir TO perfstat';
+    EXECUTE IMMEDIATE(dirSQL);
 EXCEPTION
-WHEN OTHERS THEN dbms_output.put_line(SQLERRM);
+    WHEN OTHERS THEN dbms_output.put_line(SQLERRM);
 END;
 /
+
